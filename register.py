93f72dab575d6e3fdeb2317c004629e38d5cdfe6
Javier Palacios <javiplx@gmail.com>
2010-12-04 13:06:17 +0100

    Added copyright notice and miinor fixes

--- register.py
+++ register.py
@@ -1,9 +1,19 @@
 
-import database
-import callbacks
+# Copyright (C) 2010 Javier Palacios
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License Version 2
+# as published by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
 
-from mod_python import apache
-from mod_python import util
+
+import database , callbacks
+
+from mod_python import apache , util 
 
 import time
 
