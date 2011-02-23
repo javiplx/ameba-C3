5395a69a4abf40f74cf179ba2841d91ee8a013bf
Javier Palacios <javiplx@gmail.com>
2011-02-23 12:14:56 +0100

    REFACTORING : cleaned trailing whitespaces

--- client/ameba-updater.sh
+++ client/ameba-updater.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Copyright (C) 2010 Javier Palacios
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License Version 2
 # as published by the Free Software Foundation.
-# 
+#
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
@@ -48,7 +48,7 @@ while getopts "rd:w:" opt ; do
        exit 1
        ;;
     esac
-    
+
   done
 
 eval action=\$$OPTIND
@@ -116,32 +116,32 @@ case $action in
     response=`wget -q -U "AmebaC3-Agent/${version} (shell)" -O - --header "X-AmebaStatus: ${status}" --header "Cookie: pysid=${sessid}" "${url}/logoff"`
     ;;
 
-  pull)                                                                                                                                                     
-    if [ $# -ne 0 ] ; then                                                                                                                                  
-      echo "ERROR : pull bad usage"                                                                                                                         
-      exit 1                                                                                                                                                
-      fi                                                                                                                                                    
+  pull)
+    if [ $# -ne 0 ] ; then
+      echo "ERROR : pull bad usage"
+      exit 1
+      fi
     if [ ${pull_mode} = "upgrade" ] ; then
       echo "WARNING : upgrade mode not yet available"
       status="CRITICAL"
     else
       test -n "${random_wait}" && sleep ${random_wait}
-      opkg -V 0 update && opkg -test upgrade | grep -q '^Upgrading '                                                                                          
-      if [ $? -eq 1 ] ; then                                                                                                                                  
-        status="OK"                                                                                                                                           
-      else                                                                                                                                                    
-        status="WARNING"                                                                                                                                      
-        fi                                                                                                                                                    
-      fi                                                                                                                                                    
-    url=`uci get aupd.main.url`                                                                                                                             
-    uuid=`uci get aupd.main.uuid`                                                                                                                           
-    response=`wget -q -U "AmebaC3-Agent/${version} (shell)" -O - --header "Authorization: UUID ${uuid}" "${url}/login"`                                                                           
-    set -- `echo $response | head -1`                                                                                                                       
-    if [ $# -eq 2 -a "$1" = "ID" ] ; then                                                                                                                   
-      sessid=$2                                                                                                                                             
-      fi                                                                                                                                                    
-    response=`wget -q -U "AmebaC3-Agent/${version} (shell)" -O - --header "X-AmebaStatus: ${status}" --header "Cookie: pysid=${sessid}" "${url}/logoff"`                                          
-    ;;                                                                                                                                                      
+      opkg -V 0 update && opkg -test upgrade | grep -q '^Upgrading '
+      if [ $? -eq 1 ] ; then
+        status="OK"
+      else
+        status="WARNING"
+        fi
+      fi
+    url=`uci get aupd.main.url`
+    uuid=`uci get aupd.main.uuid`
+    response=`wget -q -U "AmebaC3-Agent/${version} (shell)" -O - --header "Authorization: UUID ${uuid}" "${url}/login"`
+    set -- `echo $response | head -1`
+    if [ $# -eq 2 -a "$1" = "ID" ] ; then
+      sessid=$2
+      fi
+    response=`wget -q -U "AmebaC3-Agent/${version} (shell)" -O - --header "X-AmebaStatus: ${status}" --header "Cookie: pysid=${sessid}" "${url}/logoff"`
+    ;;
 
   *)
     print_usage ${progname}
