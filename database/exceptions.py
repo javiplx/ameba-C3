2751a00358f564744be986753822a8f7cdda6a33
Javier Palacios <javiplx@gmail.com>
2012-12-12 12:38:50 +0100

    BUGFIX : se proper message on database exceptions

--- database/exceptions.py
+++ database/exceptions.py
@@ -1,32 +1,32 @@
 
 class C3DBException ( Exception ) :
-    def __init__ ( self ) :
+    def __init__ ( self , msg ) :
         classname = str( self.__class__ )
         self.type = classname.replace( "%s." % self.__module__ , "" )
-        Exception.__init__( self )
+        Exception.__init__( self , msg )
 
 class UnknownField ( C3DBException ) :
     def __init__ ( self , field ) :
-        C3DBException.__init__( self )
-        self.message = "key '%s' unknown" % field
+        message = "key '%s' unknown" % field
+        C3DBException.__init__( self , message )
 
 class KeyExists ( C3DBException ) :
     def __init__ ( self , uuid ) :
-        C3DBException.__init__( self )
-        self.message = "'%s' already registered" % uuid
+        message = "'%s' already registered" % uuid
+        C3DBException.__init__( self , message )
 
 class KeyNotFound ( C3DBException ) :
     def __init__ ( self , uuid ) :
-        C3DBException.__init__( self )
-        self.message = "'%s' not found" % uuid
+        message = "'%s' not found" % uuid
+        C3DBException.__init__( self , message )
 
 class RecordLocked ( C3DBException ) :
     def __init__ ( self , uuid ) :
-        C3DBException.__init__( self )
-        self.message = "'%s' locked" % uuid
+        message = "'%s' locked" % uuid
+        C3DBException.__init__( self , message )
 
 class InternalError ( C3DBException ) :
     def __init__ ( self , reason ) :
-        C3DBException.__init__( self )
-        self.message = reason
+        message = reason
+        C3DBException.__init__( self , message )
 
