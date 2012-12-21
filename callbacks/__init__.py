e843b6ba44e75ac44e0e03d8bfaa6daa1a588ed4
Javier Palacios <javiplx@gmail.com>
2012-12-21 21:31:31 +0100

    Put nagios services into main configuration

--- callbacks/__init__.py
+++ callbacks/__init__.py
@@ -69,6 +69,7 @@ for path in __path__ :
 for modname in __all__ :
     module = __import__( modname , globals() )
     if config.has_section( module.callback_name ) :
+        module.amebaC3_config = config
         if not hasattr( module , 'config' ) :
             module.config = {}
         module.config.update( config.items( module.callback_name ) )
