1ca5a6523e22b723cd1124743c935ef0d97c78b5
Javier Palacios <javiplx@gmail.com>
2013-02-25 22:07:42 +0100

    Implement checkin skeleton

--- callbacks/openwrt.py
+++ callbacks/openwrt.py
@@ -28,22 +28,43 @@ wlan_mac = "00:25:9C:CF:F7:66"
 eth_mac = "00:25:9C:CF:F7:64"
 
 nodesfile = os.path.join( datadir , "data" , "%s_nodes.xml" % networkname )
-macfile = os.path.join( datadir , "data/mac2net" , "%s.txt" % base64.encodestring(wlan_mac)[:-1] )
+
+
+def addChild ( node , tagname , value=None ) :
+    doc = node.ownerDocument
+    child = node.appendChild( doc.createElement( tagname ) )
+    if value :
+        child.appendChild( doc.createTextNode( value ) )
 
 class DashboardCheckin ( __baseclass.AbstractRegisterCallback ) :
 
     name = callback_name
 
     def run ( self , uuid , dbvalues ) :
+        macaddr = dbvalues['macaddress']
         # Create nodesfile if does not exists
+        macfile = os.path.join( datadir , "data/mac2net" , "%s.txt" % base64.encodestring(macaddr)[:-1] )
         # If macfile exists, check network
-        # Else,
-        # Input params :
-        #  - location:"(lat,lon)" or lat+lon
-        #  - name
-        #  - notes
-        #  - mac
+
+        if os.path.exists( nodesfile ) :
+            doc = xml.dom.minidom.parse( nodesfile )
+        else :
+            doc = xml.dom.minidom.parseString( '<network />' )
+
+        for node in doc.getElementsByTagName('node') :
+            if macaddr == node.getElementsByTagName('mac')[0].firstChild.nodeValue :
+                print "Node %s already registered" % macaddr
+        else :
+            newnode = doc.createElement( 'node' )
+            doc.documentElement.appendChild( newnode )
+            addChild( newnode , 'name' , dbvalues['hostname'] )
+            addChild( newnode , 'notes' )
+            addChild( newnode , 'mac' , macaddr )
         # IP is not a param, but assigned by the dashboard checkin
-        # add node to nodesfile
+            addChild( newnode , 'ip' , dbvalues['hostaddress'] )
+        # location:"(lat,lon)" or lat+lon
+            addChild( newnode , 'lat' )
+            addChild( newnode , 'long' )
         # create macfile
+            doc.writexml( open( nodesfile , 'w' ) )
 
